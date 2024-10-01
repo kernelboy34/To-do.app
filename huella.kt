8e6f96a00367b56937f9942de71e82f245917ceb
kernel <86926580+kernelboy34@users.noreply.github.com>
2024-09-30 20:51:35 -0400

    Update huella.kt

--- huella.kt
+++ huella.kt
@@ -1,12 +1,21 @@
 package com.example.to_do
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import androidx.biometric.BiometricPrompt
+import androidx.biometric.BiometricManager
+import java.util.concurrent.Executor
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 class huella : AppCompatActivity() {
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    private lateinit var executor: Executor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,35 @@ class huella : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var buton=findViewById<ImageButton>(R.id.imageButtonh)
+        executor=ContextCompat.getMainExecutor(this)
+        biometricPrompt= BiometricPrompt( this,executor, object : BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(applicationContext,"Error:$errString",Toast.LENGTH_SHORT).show()
+            }
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                val intent = Intent(this@huella, log::class.java)
+                startActivity(intent)
+                Toast.makeText(applicationContext, "¡Autenticación exitosa!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(applicationContext, "Autenticación fallida", Toast.LENGTH_SHORT).show()
+            }
+        })
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Inicia sesión con tu huella")
+            .setSubtitle("Autentícate para continuar")
+            .setNegativeButtonText("Usar contraseña")
+            .build()
+
+        // Botón para iniciar la autenticación biométrica
+        buton.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
         }
     }
 }
